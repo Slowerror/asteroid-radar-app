29c6b822daca92c6a2fdb65a1897d5d257b063a7
Nikolay Zetkin <nszetkin76@gmail.com>
2022-09-17 03:13:15 +0300

    Добавил Database(v1)

--- app/src/main/java/com/slowerror/asteroidradar/models/Asteroid.kt
+++ app/src/main/java/com/slowerror/asteroidradar/models/Asteroid.kt
@@ -1,14 +1,13 @@
 package com.slowerror.asteroidradar.models
 
 import android.os.Parcelable
+import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 import kotlinx.parcelize.Parcelize
 
 @Parcelize
-@Entity
 data class Asteroid(
-    @PrimaryKey
     val id: Int,
     val codename: String,
     val closeApproachData: String,
